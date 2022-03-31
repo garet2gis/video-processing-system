ab3870b1f9ba1700ba103d538141551d2aa668b7
garet2gis <br-avm32@yandex.ru>
2022-03-31 12:53:20 +0300

    feat: add second testing player

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,6 +5,7 @@ function App() {
   return (
     <>
       <HlsPlayer source="http://localhost:8000/streams/hls1/cam1.m3u8" />
+      <HlsPlayer source="http://localhost:8000/streams/hls2/cam2.m3u8" />
     </>
   );
 }
